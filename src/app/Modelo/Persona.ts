94ba4786d8d9706638d81bc825b6d00203fc6cc3
bryan1090 <42019366+bryan1090@users.noreply.github.com>
2019-11-13 20:29:52 -0500

    archivos subidos

---
+++ src/app/Modelo/Persona.ts
@@ -0,0 +1,12 @@
+export class Persona {
+    perId: number;
+    perCedula: number;
+    perNombres: string;
+    perApellidoPaterno: string;
+    perApellidoMaterno: string;
+    perTelefonoConvencional: number;
+    perTelefonoCelular: number;
+    perCorreo: string;
+    perCiudad: string;
+    perEstadoCivil: string;
+}
